ae9a80655b5a0122fbd81b4650272b5cb70da1f7
Xinye <xinyesg@gmail.com>
2018-11-03 11:22:07 +0800

    Add calculateMaximums()

--- src/json_parser/plan_processor.py
+++ src/json_parser/plan_processor.py
@@ -3,13 +3,27 @@ from constants import NaN
 
 def get(dic, key):
     # Get value from a dic, if key not exists, set as NaN
+    if key == F.PLANS:
+        return []
     if key not in dic:
         dic[key] = NaN
     return dic[key]
 
-def process_plan(plan, explain):
+def process_plan(explain_list):
+    for e in explain_list:
+        e[F.MAXIMUM_COSTS] = 0
+        e[F.MAXIMUM_ROWS] = 0
+        e[F.MAXIMUM_DURATION] = 0
+        process(e["Plan"], e)
+
+
+def process(plan, explain):
     calculatePlannerEstimate(plan, explain)
     calculateActuals(plan, explain)
+    calculateMaximums(plan, explain)
+
+    for i in get(node, F.Plans):
+        process(i, explain)
     return plan
 
 def calculatePlannerEstimate(node, explain):
@@ -25,18 +39,27 @@ def calculateActuals(node, explain):
         node[F.ACTUAL_COST] = get(node, F.TOTAL_COST)
 
         print(node)
-        for subplan in node[F.Plans]:
-            if get(subplan, F.NODE_TYPE) !== F.CTE_SCAN:
+        for subplan in get(node, F.Plans):
+            if get(subplan, F.NODE_TYPE) != F.CTE_SCAN:
                node[F.ACTUAL_DURATION] = get(node, F.ACTUAL_DURATION) - get(subplan, F.ACTUAL_TOTAL_TIME)
                node[F.ACTUAL_COST] = get(node, F.ACTUAL_COST) - get(subplan, F.TOTAL_COST)
             
 
-        if get(node, F.ACTUAL_COST < 0:
+        if get(node, F.ACTUAL_COST) < 0:
             node[F.ACTUAL_COST] = 0
 
         node[F.ACTUAL_DURATION] = get(node, F.ACTUAL_DURATION) * get(node, F.ACTUAL_LOOPS)
         explain[F.TOTAL_COST] += get(node, F.ACTUAL_COST)
 
+def calculateMaximums(node, explain):
+    if explain[F.MAXIMUM_ROWS] < get(node, F.ACTUAL_ROWS):
+        explain[F.MAXIMUM_ROWS] = get(node, F.ACTUAL_ROWS)
+
+    if explain[F.MAXIMUM_COSTS] < get(node, F.ACTUAL_COST):
+        explain[F.MAXIMUM_ROWS] = get(node, F.ACTUAL_COST)
+
+    if explain[F.MAXIMUM_DURATION] < get(node, F.ACTUAL_DURATION):
+        explain[F.MAXIMUM_DURATION] = get(node, F.ACTUAL_DURATION)
 
 if __name_- == '__main__':
     pass
